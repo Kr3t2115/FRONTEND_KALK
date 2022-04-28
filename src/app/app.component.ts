76dc11f9d9f8e013dd3e6ba567007c668bb04f11
Kr3t2115 <szymon.baranowicz@teb.edu.pl>
2022-04-28 22:41:09 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import {HttpClient,HttpClientModule} from '@angular/common/http';
+
+interface liczenie {
+  id: any;
+  pierwsza_liczba: any;
+  operator: any;
+  druga_liczba: any;
+  wynik_dzialania: any;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private http:HttpClient) { }
+
+  all: any;
+
+  link = 'http://localhost:8000/api/obliczenia/?format=json';
+
+  ngOnInit(){
+    this.http.get<liczenie>(this.link).subscribe(
+      (res) => {
+      this.all = res;
+    })
+  }
+  
 }
